edf72f78247a0dab645d2b212706d1ce38115c71
ne_lox <liviolettassfinger@gmail.com>
2022-12-12 03:01:51 +0400

    lol

--- site/app.py
+++ site/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def index():
+    return render_template("number/number.html")
+
+if __name__ == "__main__":
+    app.run()
 
---
+++ site/static/script.js
@@ -0,0 +1,51 @@
+countries =
+$.fn.focusEnd = function() {
+    $(this).focus();
+    var tmp = $('<span />').appendTo($(this)),
+        node = tmp.get(0),
+        range = null,
+        sel = null;
+
+    if (document.selection) {
+        range = document.body.createTextRange();
+        range.moveToElementText(node);
+        range.select();
+    } else if (window.getSelection) {
+        range = document.createRange();
+        range.selectNode(node);
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+    tmp.remove();
+    return this;
+}
+function isDigit(s){
+    if (isNaN(Number(s)) == true){
+        return false
+    }
+    if (s.indexOf(" ")==0 || s.indexOf(".")==0){
+        return false
+    }
+    return true
+}
+last_value = "+"
+$(document).ready(function() {
+    $('#phone').on('input', function() {
+        phone = document.getElementById("phone");
+        if (last_value.length > phone.innerHTML.length){
+            if (phone.innerHTML[0] != "+"){
+                phone.innerHTML = last_value;
+                $('#phone').focusEnd();
+            }
+        }
+        if (last_value.length < phone.innerHTML.length){
+            if (isDigit(phone.innerHTML) == false || phone.innerHTML.length > 12){
+                phone.innerHTML = last_value;
+                $('#phone').focusEnd();
+            }
+        }
+
+        last_value = phone.innerHTML;
+    });
+});
